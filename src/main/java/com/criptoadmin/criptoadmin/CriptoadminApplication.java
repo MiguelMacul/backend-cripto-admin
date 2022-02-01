994ae969fab5b5690c9cf03ad0dcbada63dc9876
Miguel Macuil <miguelmacuil@outlook.es>
2022-01-31 18:37:44 -0600

    struct base of proyect in java with springboot

---
+++ src/main/java/com/criptoadmin/criptoadmin/CriptoadminApplication.java
@@ -0,0 +1,13 @@
+package com.criptoadmin.criptoadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/criptoadmin/criptoadmin/CriptoadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criptoadmin.criptoadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
